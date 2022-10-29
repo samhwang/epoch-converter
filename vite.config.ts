47b82a6686ea24fe39a1c844ce9587bbc6bebf5e
Sam Huynh <sam@hungryhungry.com>
2022-10-30 09:09:20 +1100

    Setup tauri

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,17 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  clearScreen: false,
+  server: {
+    strictPort: true,
+    port: 5173,
+  },
+  envPrefix: ['VITE_', 'TAURI_'],
+  build: {
+    target: ['es2021', 'chrome100', 'safari13'],
+    minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
+    sourcemap: !!process.env.TAURI_DEBUG,
+  },
   test: {
     globals: true,
     environment: 'jsdom',
